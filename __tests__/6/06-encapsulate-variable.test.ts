716e9f3d5b06c90751a15d23d56d2421978c3814
Ashe <tech@ashe.kr>
2022-01-29 16:31:49 +0900

    06.06-encapsulate-variable.ts

---
+++ __tests__/6/06-encapsulate-variable.test.ts
@@ -0,0 +1,19 @@
+import {createSpaceShip, changeDefaultOwner, defaultOwner} from '../../src/6/06-encapsulate-variable'
+import {expect, describe, it} from '@jest/globals'
+
+
+describe('Test encapsulate', () => {
+    it('defaultOwner', async () => {
+        expect(defaultOwner).toEqual({firstName: '마틴', lastName: '파울러'})
+    })
+
+    it('createSpaceShip', async () => {
+        const spaceship = createSpaceShip()
+        expect(spaceship.owner).toEqual(defaultOwner)
+    })
+
+    it('changeDefaultOwner', async () => {
+        changeDefaultOwner()
+        expect(defaultOwner).toEqual({firstName: '레베카', lastName: '파슨스'})
+    })
+})
