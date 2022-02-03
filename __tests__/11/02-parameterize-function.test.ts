9d77b25ffbbdcd912686ddc75f5103e2b6913901
Ashe <tech@ashe.kr>
2022-02-03 20:46:20 +0900

    11.02

---
+++ __tests__/11/02-parameterize-function.test.ts
@@ -0,0 +1,9 @@
+import {baseCharge} from '../../src/11/02-parameterize-function'
+import {expect, describe, it} from '@jest/globals'
+
+
+describe('Test 11.02. Parameterize Function', () => {
+    it('baseCharge', async () => {
+        expect(baseCharge(250)).toEqual(11.5)
+    })
+})
